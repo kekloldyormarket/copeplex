fe114df1c4e2e470cfd260f00e7b2efc45445ffe
Your Name <you@example.com>
2021-10-26 19:24:49 -0300

    Updates

---
+++ _next/static/5YJ9YvGdlKkgmoxbqJ2IW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-c193a0c90033bdbeb9b8.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
